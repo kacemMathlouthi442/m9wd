6bf8129a3e29ee8d3128b92faf5e95e6c74735b6
kacemMathlouthi442 <wmathlouthi661@gmail.com>
2025-11-16 13:21:40 +0100

    Add files via upload

---
+++ config/links_ids.py
@@ -0,0 +1,14 @@
+def get_groups():
+    new_users_ID = -1003235649632
+    redeemed_keys_ID = -1003333883684
+    vouches_LINK, vouches_ID = 'https://t.me/M9WDVOUCHERS', -1003469369405
+    main_channel_link, main_channel_ID = 'https://t.me/M9WDOTPOTP', -1002269511043
+
+    return {
+        "new_users_ID": new_users_ID,
+        "redeemed_keys_ID": redeemed_keys_ID,
+        "vouches_LINK": vouches_LINK,
+        "vouches_ID": vouches_ID,
+        "main_channel_link": main_channel_link,
+        "main_channel_ID": main_channel_ID,
+    }
---
+++ config/stickers.py
@@ -0,0 +1,4 @@
+def get_image():
+    return "AgACAgQAAxkDAAICG2kZvDd2gkzo5wIDie8_gT2TdBoSAAJhC2sbd5XQUEf9WagvpSlBAQADAgADeAADNgQ"
+
+
